85869fb5d2f6492391270ab169e89c0e4c308353
Joe Renteria <joe@electronflight.com>
2021-09-13 01:52:14 -0500

    commit tree 6668ee54b64ae6699a355dda39d8ae6be1272947

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,23 @@
 class ApplicationController < Sinatra::Base
+    set :default_content_type,"application/json"
 
-  # add routes
+    get '/bakeries' do
+        bakeries = Bakery.all
+        bakeries.to_json
+    end
 
+    get '/bakeries/:id' do
+        bakeries = Bakery.find(params[:id])
+        bakeries.to_json
+    end
+
+    get '/baked_goods/by_price' do
+        bakeries = Bakery.all.order(:price)
+        bakeries.to_json
+    end
+
+    get '/baked_goods/most_expensive' do
+        bakeries = Bakery.all.order(:price).max
+        bakeries.to_json
+    end
 end
